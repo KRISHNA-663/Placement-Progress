33add648d54733e69404d256572a814d681aabe8
KRISHNA <krish7cbe@gmail.com>
2023-07-24 21:21:26 +0530

    all files

---
+++ Reverse_butterfly_aka_rhombus.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int num,row,col;
+    scanf("%d",&num);
+    for(row=num;row>0;row--,printf("\n")){
+        for(col=1;col<=row;col++){
+            printf("*");
+        }
+        for(col=1;col<=num-row;col++){
+            printf(" ");
+        }
+        for(col=1;col<=num-row;col++){
+            printf(" ");
+        }
+        for(col=1;col<=row;col++){
+            printf("*");
+        }
+}
+    for(row=1;row<=num;row++,printf("\n")){
+        for(col=1;col<=row;col++){
+            printf("*");
+        }
+        for(col=1;col<=num-row;col++){
+            printf(" ");
+        }
+        for(col=1;col<=num-row;col++){
+            printf(" ");
+        }
+        for(col=1;col<=row;col++){
+            printf("*");
+        }
+}
+
+}
