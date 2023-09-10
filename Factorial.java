459636950e57274a65f74f7ba10a844600bd7689
KRISHNA-663 <krish7cbe@gmail.com>
2023-09-10 14:50:10 +0530

    changes

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial{
+    public static void main(String[]args)
+    {
+        int a=3;
+        int b=1;
+        for(int i=1;i<=a;i++)
+        {
+            b=b*i;
+        }
+        System.out.print("Factorial of "+a+" is "+b);
+    }
+}
+
